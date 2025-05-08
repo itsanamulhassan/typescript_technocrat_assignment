5123a06ed1a143e63ea32f142fe81410c6082b0c
Anamul Hassan <itsanamulhassan@gmail.com>
2025-05-08 21:45:40 +0600

    solve problem 2

--- index.ts
+++ index.ts
@@ -3,3 +3,11 @@ function formatString(input: string, toUpper?: boolean): string {
     ? input.toUpperCase()
     : input.toLowerCase();
 }
+
+interface Book {
+  title: string;
+  rating: number;
+}
+function filterByRating(items: Book[]): Book[] {
+  return items.filter((singleItem: Book) => singleItem.rating >= 4);
+}
